ea60dbbd5530a5823c5c2578449a04a19f52dd39
Sana2010 <78859417+Sana2010@users.noreply.github.com>
2021-07-25 22:44:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+img = "";
+
+status = "";
+
+objects = [];
+
+function setup()
+{
+canvas = createCanvas(380,380);
+canvas.center(); 
+
+video = createCapture(VIDEO);
+video.hide();
+
+objectDetector = ml5.objectDetector('cocoassd',modelLoaded);
+document.getElementById("status").innerHTML = "Status : Baby not found";
+}
+
+function modelLoaded()
+{
+console.log("Model Loaded")
+status = true;
+}
+
+function gotResult(error,results)
+{
+if(error)
+{
+console.log(error);
+}
+
+console.log(results);
+
+objects = results;
+}
+
+function draw()
+{
+image(video,0,0,380,380);
+
+if(status != "")
+{
+r = random(255);
+g = random(255);
+b = random(255);
+
+objectDetector.detect(video,gotResult);
+
+for(i = 0; i < objects.length; i++)
+{
+document.getElementById("status").innerHTML = "Baby Found";
+
+document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : " + objects.length;
+
+fill(r,g,b);
+percent = floor(objects[i].confidence * 100);
+text(objects[i].label + " " + percent + "%",objects[i].x , objects[i].y);
+noFill();
+stroke(r,g,b);
+rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height);
+}
+}
+}
